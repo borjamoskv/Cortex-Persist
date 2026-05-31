8fa6b78a9b1af4f3ecbca94de2d4cee65e4224ad
Tester <tester@cortex.com>
2026-05-31 08:47:09 +0200

    refactor(sica): amputate autonomy.py god object into modular components

---
+++ cortex/sica/autonomy/__init__.py
@@ -0,0 +1,13 @@
+from .speculative_fork import SpeculativeFork, ForkResult
+from .trace_synthesizer import TraceSynthesizer
+from .meta_meta_controller import MetaMetaController, MetaMetaDiagnosis
+from .adaptive_retry import AdaptiveRetry
+from .autonomous_tick import AutonomousTick
+
+__all__ = [
+    "SpeculativeFork", "ForkResult",
+    "TraceSynthesizer",
+    "MetaMetaController", "MetaMetaDiagnosis",
+    "AdaptiveRetry",
+    "AutonomousTick",
+]
