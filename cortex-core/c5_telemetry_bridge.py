34c16bf846724aba35e1b6c7e945e497724d7392
Tester <tester@cortex.com>
2026-06-06 08:18:03 +0200

    feat(telemetry): implement interactive ultramap-omega visualizer and stream control vector

--- cortex-core/c5_telemetry_bridge.py
+++ cortex-core/c5_telemetry_bridge.py
@@ -38,7 +38,11 @@ class TelemetryBridge:
                             "y": round(agent_state['y'], 3),
                             "z": round(agent_state['z'], 3),
                             "target": agent_state['target'],
-                            "entropy": round(agent_state['entropy'], 3)
+                            "entropy": round(agent_state['entropy'], 3),
+                            "queue_depth": round(agent_state.get('queue_depth', 0.0), 3),
+                            "error_rate": round(agent_state.get('error_rate', 0.0), 3),
+                            "causal_entropy": round(agent_state.get('causal_entropy', 0.0), 3),
+                            "cpu_load": round(agent_state.get('cpu_load', 0.0), 3)
                         })
                 
                 if state_frame:
