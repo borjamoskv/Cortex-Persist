1202ca2f2b748ca6df465fb52ecf926601b1eb6e
Borja Moskv <82632764+borjamoskv@users.noreply.github.com>
2026-07-02 10:56:06 +0200

    perf(lisp-engine): unroll OBB SAT loop to achieve absolute zero consing (0 bytes allocated)

--- babylon60/extensions/lisp_engine/primitives.lisp
+++ babylon60/extensions/lisp_engine/primitives.lisp
@@ -47,141 +47,161 @@
          (and (<= (aref a-min 2) (aref b-max 2)) (>= (aref a-max 2) (aref b-min 2))))))
 
 ;; 2. Intersección OBB vs OBB usando Separating Axis Theorem (SAT)
-;; Implementado sin consing en tiempo de ejecución (cero garbage collection)
+;; Implementación unrolled para garantizar cero consing (0 bytes allocated en Heap)
 (defun intersect-obb-obb-p (a b)
   (declare (type obb a b)
            (optimize (speed 3) (safety 0)))
-  (let ((t-diff (make-vec3))
-        (a-center (obb-center a))
-        (b-center (obb-center b))
-        (a-axes (obb-axes a))
-        (b-axes (obb-axes b))
-        (a-extents (obb-extents a))
-        (b-extents (obb-extents b))
-        ;; Variables locales para almacenar columnas y evitar consing
-        (a-u0 (make-vec3)) (a-u1 (make-vec3)) (a-u2 (make-vec3))
-        (b-u0 (make-vec3)) (b-u1 (make-vec3)) (b-u2 (make-vec3)))
-    (declare (dynamic-extent t-diff a-u0 a-u1 a-u2 b-u0 b-u1 b-u2))
+  (let* ((a-center (obb-center a))
+         (b-center (obb-center b))
+         (a-axes (obb-axes a))
+         (b-axes (obb-axes b))
+         (a-extents (obb-extents a))
+         (b-extents (obb-extents b))
+         
+         ;; Diferencia de centros
+         (t-x (- (aref b-center 0) (aref a-center 0)))
+         (t-y (- (aref b-center 1) (aref a-center 1)))
+         (t-z (- (aref b-center 2) (aref a-center 2)))
+         
+         ;; Extraer componentes de los ejes de A (columnas de la matriz)
+         (a-u0-x (aref a-axes 0)) (a-u0-y (aref a-axes 3)) (a-u0-z (aref a-axes 6))
+         (a-u1-x (aref a-axes 1)) (a-u1-y (aref a-axes 4)) (a-u1-z (aref a-axes 7))
+         (a-u2-x (aref a-axes 2)) (a-u2-y (aref a-axes 5)) (a-u2-z (aref a-axes 8))
+         
+         ;; Extraer componentes de los ejes de B (columnas de la matriz)
+         (b-u0-x (aref b-axes 0)) (b-u0-y (aref b-axes 3)) (b-u0-z (aref b-axes 6))
+         (b-u1-x (aref b-axes 1)) (b-u1-y (aref b-axes 4)) (b-u1-z (aref b-axes 7))
+         (b-u2-x (aref b-axes 2)) (b-u2-y (aref b-axes 5)) (b-u2-z (aref b-axes 8)))
     
-    ;; Extraer ejes locales
-    (get-column a-u0 a-axes 0)
-    (get-column a-u1 a-axes 1)
-    (get-column a-u2 a-axes 2)
-    (get-column b-u0 b-axes 0)
-    (get-column b-u1 b-axes 1)
-    (get-column b-u2 b-axes 2)
+    (declare (type single-float t-x t-y t-z
+                   a-u0-x a-u0-y a-u0-z a-u1-x a-u1-y a-u1-z a-u2-x a-u2-y a-u2-z
+                   b-u0-x b-u0-y b-u0-z b-u1-x b-u1-y b-u1-z b-u2-x b-u2-y b-u2-z))
     
-    ;; Calcular vector de traducción entre centros
-    (vec-subtract t-diff b-center a-center)
-    
-    (let ((r (make-array (quote (3 3)) :element-type (quote single-float)))
-          (abs-r (make-array (quote (3 3)) :element-type (quote single-float))))
-      (declare (dynamic-extent r abs-r))
+    ;; Calcular matriz de rotación relativa R_ij = dot(A_i, B_j)
+    (let* ((r00 (+ (* a-u0-x b-u0-x) (* a-u0-y b-u0-y) (* a-u0-z b-u0-z)))
+           (r01 (+ (* a-u0-x b-u1-x) (* a-u0-y b-u1-y) (* a-u0-z b-u1-z)))
+           (r02 (+ (* a-u0-x b-u2-x) (* a-u0-y b-u2-y) (* a-u0-z b-u2-z)))
+           
+           (r10 (+ (* a-u1-x b-u0-x) (* a-u1-y b-u0-y) (* a-u1-z b-u0-z)))
+           (r11 (+ (* a-u1-x b-u1-x) (* a-u1-y b-u1-y) (* a-u1-z b-u1-z)))
+           (r12 (+ (* a-u1-x b-u2-x) (* a-u1-y b-u2-y) (* a-u1-z b-u2-z)))
+           
+           (r20 (+ (* a-u2-x b-u0-x) (* a-u2-y b-u0-y) (* a-u2-z b-u0-z)))
+           (r21 (+ (* a-u2-x b-u1-x) (* a-u2-y b-u1-y) (* a-u2-z b-u1-z)))
+           (r22 (+ (* a-u2-x b-u2-x) (* a-u2-y b-u2-y) (* a-u2-z b-u2-z)))
+           
+           ;; Valores absolutos de la matriz R con epsilon para evitar singularidades
+           (ar00 (+ (abs r00) 1.0f-6)) (ar01 (+ (abs r01) 1.0f-6)) (ar02 (+ (abs r02) 1.0f-6))
+           (ar10 (+ (abs r10) 1.0f-6)) (ar11 (+ (abs r11) 1.0f-6)) (ar12 (+ (abs r12) 1.0f-6))
+           (ar20 (+ (abs r20) 1.0f-6)) (ar21 (+ (abs r21) 1.0f-6)) (ar22 (+ (abs r22) 1.0f-6)))
       
-      ;; Calcular matriz de rotación relativa
-      (loop for i from 0 to 2 do
-           (loop for j from 0 to 2 do
-                (let ((axes-a (cond ((= i 0) a-u0) ((= i 1) a-u1) (t a-u2)))
-                      (axes-b (cond ((= j 0) b-u0) ((= j 1) b-u1) (t b-u2))))
-                  (let ((val (dot-product axes-a axes-b)))
-                    (setf (aref r i j) val)
-                    (setf (aref abs-r i j) (+ (abs val) 1.0f-6)))))) ; Añadir epsilon
+      (declare (type single-float r00 r01 r02 r10 r11 r12 r20 r21 r22
+                     ar00 ar01 ar02 ar10 ar11 ar12 ar20 ar21 ar22))
       
-      (let ((ra 0.0f0) (rb 0.0f0))
-        (declare (type single-float ra rb))
+      (let ((ra 0.0f0) (rb 0.0f0) (t-val 0.0f0))
+        (declare (type single-float ra rb t-val))
         
-        ;; Test de los 3 ejes de A
-        (loop for i from 0 to 2 do
-             (let ((axes-a (cond ((= i 0) a-u0) ((= i 1) a-u1) (t a-u2))))
-               (setf ra (aref a-extents i))
-               (setf rb (+ (* (aref b-extents 0) (aref abs-r i 0))
-                           (* (aref b-extents 1) (aref abs-r i 1))
-                           (* (aref b-extents 2) (aref abs-r i 2))))
-               (when (> (abs (dot-product t-diff axes-a)) (+ ra rb))
-                 (return-from intersect-obb-obb-p nil))))
+        ;; 1. Test de los 3 ejes de A
+        ;; Eje A0
+        (setf ra (aref a-extents 0))
+        (setf rb (+ (* (aref b-extents 0) ar00) (* (aref b-extents 1) ar01) (* (aref b-extents 2) ar02)))
+        (setf t-val (abs (+ (* t-x a-u0-x) (* t-y a-u0-y) (* t-z a-u0-z))))
+        (when (> t-val (+ ra rb)) (return-from intersect-obb-obb-p nil))
         
-        ;; Test de los 3 ejes de B
-        (loop for j from 0 to 2 do
-             (let ((axes-b (cond ((= j 0) b-u0) ((= j 1) b-u1) (t b-u2))))
-               (setf ra (+ (* (aref a-extents 0) (aref abs-r 0 j))
-                           (* (aref a-extents 1) (aref abs-r 1 j))
-                           (* (aref a-extents 2) (aref abs-r 2 j))))
-               (setf rb (aref b-extents j))
-               (when (> (abs (dot-product t-diff axes-b)) (+ ra rb))
-                 (return-from intersect-obb-obb-p nil))))
+        ;; Eje A1
+        (setf ra (aref a-extents 1))
+        (setf rb (+ (* (aref b-extents 0) ar10) (* (aref b-extents 1) ar11) (* (aref b-extents 2) ar12)))
+        (setf t-val (abs (+ (* t-x a-u1-x) (* t-y a-u1-y) (* t-z a-u1-z))))
+        (when (> t-val (+ ra rb)) (return-from intersect-obb-obb-p nil))
         
-        ;; Test de los 9 ejes cruzados
+        ;; Eje A2
+        (setf ra (aref a-extents 2))
+        (setf rb (+ (* (aref b-extents 0) ar20) (* (aref b-extents 1) ar21) (* (aref b-extents 2) ar22)))
+        (setf t-val (abs (+ (* t-x a-u2-x) (* t-y a-u2-y) (* t-z a-u2-z))))
+        (when (> t-val (+ ra rb)) (return-from intersect-obb-obb-p nil))
+        
+        ;; 2. Test de los 3 ejes de B
+        ;; Eje B0
+        (setf ra (+ (* (aref a-extents 0) ar00) (* (aref a-extents 1) ar10) (* (aref a-extents 2) ar20)))
+        (setf rb (aref b-extents 0))
+        (setf t-val (abs (+ (* t-x b-u0-x) (* t-y b-u0-y) (* t-z b-u0-z))))
+        (when (> t-val (+ ra rb)) (return-from intersect-obb-obb-p nil))
+        
+        ;; Eje B1
+        (setf ra (+ (* (aref a-extents 0) ar01) (* (aref a-extents 1) ar11) (* (aref a-extents 2) ar21)))
+        (setf rb (aref b-extents 1))
+        (setf t-val (abs (+ (* t-x b-u1-x) (* t-y b-u1-y) (* t-z b-u1-z))))
+        (when (> t-val (+ ra rb)) (return-from intersect-obb-obb-p nil))
+        
+        ;; Eje B2
+        (setf ra (+ (* (aref a-extents 0) ar02) (* (aref a-extents 1) ar12) (* (aref a-extents 2) ar22)))
+        (setf rb (aref b-extents 2))
+        (setf t-val (abs (+ (* t-x b-u2-x) (* t-y b-u2-y) (* t-z b-u2-z))))
+        (when (> t-val (+ ra rb)) (return-from intersect-obb-obb-p nil))
+        
+        ;; 3. Test de los 9 ejes cruzados
         ;; Eje A0 x B0
-        (setf ra (+ (* (aref a-extents 1) (aref abs-r 2 0)) (* (aref a-extents 2) (aref abs-r 1 0))))
-        (setf rb (+ (* (aref b-extents 1) (aref abs-r 0 2)) (* (aref b-extents 2) (aref abs-r 0 1))))
-        (when (> (abs (- (* (dot-product t-diff a-u2) (aref r 1 0))
-                         (* (dot-product t-diff a-u1) (aref r 2 0))))
-                 (+ ra rb))
-          (return-from intersect-obb-obb-p nil))
+        (setf ra (+ (* (aref a-extents 1) ar20) (* (aref a-extents 2) ar10)))
+        (setf rb (+ (* (aref b-extents 1) ar02) (* (aref b-extents 2) ar01)))
+        (setf t-val (abs (- (* (+ (* t-x a-u2-x) (* t-y a-u2-y) (* t-z a-u2-z)) r10)
+                            (* (+ (* t-x a-u1-x) (* t-y a-u1-y) (* t-z a-u1-z)) r20))))
+        (when (> t-val (+ ra rb)) (return-from intersect-obb-obb-p nil))
         
         ;; Eje A0 x B1
-        (setf ra (+ (* (aref a-extents 1) (aref abs-r 2 1)) (* (aref a-extents 2) (aref abs-r 1 1))))
-        (setf rb (+ (* (aref b-extents 0) (aref abs-r 0 2)) (* (aref b-extents 2) (aref abs-r 0 0))))
-        (when (> (abs (- (* (dot-product t-diff a-u2) (aref r 1 1))
-                         (* (dot-product t-diff a-u1) (aref r 2 1))))
-                 (+ ra rb))
-          (return-from intersect-obb-obb-p nil))
+        (setf ra (+ (* (aref a-extents 1) ar21) (* (aref a-extents 2) ar11)))
+        (setf rb (+ (* (aref b-extents 0) ar02) (* (aref b-extents 2) ar00)))
+        (setf t-val (abs (- (* (+ (* t-x a-u2-x) (* t-y a-u2-y) (* t-z a-u2-z)) r11)
+                            (* (+ (* t-x a-u1-x) (* t-y a-u1-y) (* t-z a-u1-z)) r22))))
+        (when (> t-val (+ ra rb)) (return-from intersect-obb-obb-p nil))
         
         ;; Eje A0 x B2
-        (setf ra (+ (* (aref a-extents 1) (aref abs-r 2 2)) (* (aref a-extents 2) (aref abs-r 1 2))))
-        (setf rb (+ (* (aref b-extents 0) (aref abs-r 0 1)) (* (aref b-extents 1) (aref abs-r 0 0))))
-        (when (> (abs (- (* (dot-product t-diff a-u2) (aref r 1 2))
-                         (* (dot-product t-diff a-u1) (aref r 2 2))))
-                 (+ ra rb))
-          (return-from intersect-obb-obb-p nil))
+        (setf ra (+ (* (aref a-extents 1) ar22) (* (aref a-extents 2) ar12)))
+        (setf rb (+ (* (aref b-extents 0) ar01) (* (aref b-extents 1) ar00)))
+        (setf t-val (abs (- (* (+ (* t-x a-u2-x) (* t-y a-u2-y) (* t-z a-u2-z)) r12)
+                            (* (+ (* t-x a-u1-x) (* t-y a-u1-y) (* t-z a-u1-z)) r21))))
+        (when (> t-val (+ ra rb)) (return-from intersect-obb-obb-p nil))
         
         ;; Eje A1 x B0
-        (setf ra (+ (* (aref a-extents 0) (aref abs-r 2 0)) (* (aref a-extents 2) (aref abs-r 0 0))))
-        (setf rb (+ (* (aref b-extents 1) (aref abs-r 1 2)) (* (aref b-extents 2) (aref abs-r 1 1))))
-        (when (> (abs (- (* (dot-product t-diff a-u0) (aref r 2 0))
-                         (* (dot-product t-diff a-u2) (aref r 0 0))))
-                 (+ ra rb))
-          (return-from intersect-obb-obb-p nil))
+        (setf ra (+ (* (aref a-extents 0) ar20) (* (aref a-extents 2) ar00)))
+        (setf rb (+ (* (aref b-extents 1) ar12) (* (aref b-extents 2) ar11)))
+        (setf t-val (abs (- (* (+ (* t-x a-u0-x) (* t-y a-u0-y) (* t-z a-u0-z)) r20)
+                            (* (+ (* t-x a-u2-x) (* t-y a-u2-y) (* t-z a-u2-z)) r00))))
+        (when (> t-val (+ ra rb)) (return-from intersect-obb-obb-p nil))
         
         ;; Eje A1 x B1
-        (setf ra (+ (* (aref a-extents 0) (aref abs-r 2 1)) (* (aref a-extents 2) (aref abs-r 0 1))))
-        (setf rb (+ (* (aref b-extents 0) (aref abs-r 1 2)) (* (aref b-extents 2) (aref abs-r 1 0))))
-        (when (> (abs (- (* (dot-product t-diff a-u0) (aref r 2 1))
-                         (* (dot-product t-diff a-u2) (aref r 0 1))))
-                 (+ ra rb))
-          (return-from intersect-obb-obb-p nil))
+        (setf ra (+ (* (aref a-extents 0) ar21) (* (aref a-extents 2) ar01)))
+        (setf rb (+ (* (aref b-extents 0) ar12) (* (aref b-extents 2) ar10)))
+        (setf t-val (abs (- (* (+ (* t-x a-u0-x) (* t-y a-u0-y) (* t-z a-u0-z)) r21)
+                            (* (+ (* t-x a-u2-x) (* t-y a-u2-y) (* t-z a-u2-z)) r01))))
+        (when (> t-val (+ ra rb)) (return-from intersect-obb-obb-p nil))
         
         ;; Eje A1 x B2
-        (setf ra (+ (* (aref a-extents 0) (aref abs-r 2 2)) (* (aref a-extents 2) (aref abs-r 0 2))))
-        (setf rb (+ (* (aref b-extents 0) (aref abs-r 1 1)) (* (aref b-extents 1) (aref abs-r 1 0))))
-        (when (> (abs (- (* (dot-product t-diff a-u0) (aref r 2 2))
-                         (* (dot-product t-diff a-u2) (aref r 0 2))))
-                 (+ ra rb))
-          (return-from intersect-obb-obb-p nil))
+        (setf ra (+ (* (aref a-extents 0) ar22) (* (aref a-extents 2) ar02)))
+        (setf rb (+ (* (aref b-extents 0) ar11) (* (aref b-extents 1) ar10)))
+        (setf t-val (abs (- (* (+ (* t-x a-u0-x) (* t-y a-u0-y) (* t-z a-u0-z)) r22)
+                            (* (+ (* t-x a-u2-x) (* t-y a-u2-y) (* t-z a-u2-z)) r02))))
+        (when (> t-val (+ ra rb)) (return-from intersect-obb-obb-p nil))
         
         ;; Eje A2 x B0
-        (setf ra (+ (* (aref a-extents 0) (aref abs-r 1 0)) (* (aref a-extents 1) (aref abs-r 0 0))))
-        (setf rb (+ (* (aref b-extents 1) (aref abs-r 2 2)) (* (aref b-extents 2) (aref abs-r 2 1))))
-        (when (> (abs (- (* (dot-product t-diff a-u1) (aref r 0 0))
-                         (* (dot-product t-diff a-u0) (aref r 1 0))))
-                 (+ ra rb))
-          (return-from intersect-obb-obb-p nil))
+        (setf ra (+ (* (aref a-extents 0) ar10) (* (aref a-extents 1) ar00)))
+        (setf rb (+ (* (aref b-extents 1) ar22) (* (aref b-extents 2) ar21)))
+        (setf t-val (abs (- (* (+ (* t-x a-u1-x) (* t-y a-u1-y) (* t-z a-u1-z)) r00)
+                            (* (+ (* t-x a-u0-x) (* t-y a-u0-y) (* t-z a-u0-z)) r10))))
+        (when (> t-val (+ ra rb)) (return-from intersect-obb-obb-p nil))
         
         ;; Eje A2 x B1
-        (setf ra (+ (* (aref a-extents 0) (aref abs-r 1 1)) (* (aref a-extents 1) (aref abs-r 0 1))))
-        (setf rb (+ (* (aref b-extents 0) (aref abs-r 2 2)) (* (aref b-extents 2) (aref abs-r 2 0))))
-        (when (> (abs (- (* (dot-product t-diff a-u1) (aref r 0 1))
-                         (* (dot-product t-diff a-u0) (aref r 1 1))))
-                 (+ ra rb))
-          (return-from intersect-obb-obb-p nil))
+        (setf ra (+ (* (aref a-extents 0) ar11) (* (aref a-extents 1) ar01)))
+        (setf rb (+ (* (aref b-extents 0) ar22) (* (aref b-extents 2) ar20)))
+        (setf t-val (abs (- (* (+ (* t-x a-u1-x) (* t-y a-u1-y) (* t-z a-u1-z)) r01)
+                            (* (+ (* t-x a-u0-x) (* t-y a-u0-y) (* t-z a-u0-z)) r11))))
+        (when (> t-val (+ ra rb)) (return-from intersect-obb-obb-p nil))
         
         ;; Eje A2 x B2
-        (setf ra (+ (* (aref a-extents 0) (aref abs-r 1 2)) (* (aref a-extents 1) (aref abs-r 0 2))))
-        (setf rb (+ (* (aref b-extents 0) (aref abs-r 2 1)) (* (aref b-extents 1) (aref abs-r 2 0))))
-        (when (> (abs (- (* (dot-product t-diff a-u1) (aref r 0 2))
-                         (* (dot-product t-diff a-u0) (aref r 1 2))))
-                 (+ ra rb))
-          (return-from intersect-obb-obb-p nil))
+        (setf ra (+ (* (aref a-extents 0) ar12) (* (aref a-extents 1) ar02)))
+        (setf rb (+ (* (aref b-extents 0) ar21) (* (aref b-extents 1) ar20)))
+        (setf t-val (abs (- (* (+ (* t-x a-u1-x) (* t-y a-u1-y) (* t-z a-u1-z)) r02)
+                            (* (+ (* t-x a-u0-x) (* t-y a-u0-y) (* t-z a-u0-z)) r12))))
+        (when (> t-val (+ ra rb)) (return-from intersect-obb-obb-p nil))
         
         t))))
 
@@ -192,38 +212,43 @@
            (optimize (speed 3) (safety 0)))
   (let ((t-min 0.0f0)
         (t-max most-positive-single-float)
-        (p (make-vec3))
         (o-center (obb-center o))
         (o-axes (obb-axes o))
         (o-extents (obb-extents o))
         (ray-origin (ray-origin ry))
-        (ray-direction (ray-direction ry))
-        (u (make-vec3)))
-    (declare (type single-float t-min t-max)
-             (dynamic-extent p u))
+        (ray-direction (ray-direction ry)))
+    (declare (type single-float t-min t-max))
     
-    (vec-subtract p o-center ray-origin)
-    
-    (loop for i from 0 to 2 do
-         (get-column u o-axes i)
-         (let ((e (dot-product u p))
-               (f (dot-product u ray-direction))
-               (extent (aref o-extents i)))
-           (declare (type single-float e f extent))
-           (if (> (abs f) 1.0f-6)
-               (let ((t1 (/ (+ e extent) f))
-                     (t2 (/ (- e extent) f)))
-                 (declare (type single-float t1 t2))
-                 (when (> t1 t2)
-                   (rotatef t1 t2))
-                 (when (> t1 t-min)
-                   (setf t-min t1))
-                 (when (< t2 t-max)
-                   (setf t-max t2))
-                 (when (> t-min t-max)
-                   (return-from intersect-ray-obb-p nil))
-                 (when (< t-max 0.0f0)
-                   (return-from intersect-ray-obb-p nil)))
-               (when (or (< (+ e extent) 0.0f0) (> (- e extent) 0.0f0))
-                 (return-from intersect-ray-obb-p nil)))))
-    t))
+    (let ((p-x (- (aref ray-origin 0) (aref o-center 0)))
+          (p-y (- (aref ray-origin 1) (aref o-center 1)))
+          (p-z (- (aref ray-origin 2) (aref o-center 2))))
+      (declare (type single-float p-x p-y p-z))
+      
+      (loop for i from 0 to 2 do
+           (let ((u-x (aref o-axes (+ i 0)))
+                 (u-y (aref o-axes (+ i 3)))
+                 (u-z (aref o-axes (+ i 6)))
+                 (extent (aref o-extents i)))
+             (declare (type single-float u-x u-y u-z extent))
+             (let ((e (+ (* u-x p-x) (* u-y p-y) (* u-z p-z)))
+                   (f (+ (* u-x (aref ray-direction 0))
+                         (* u-y (aref ray-direction 1))
+                         (* u-z (aref ray-direction 2)))))
+               (declare (type single-float e f))
+               (if (> (abs f) 1.0f-6)
+                   (let ((t1 (/ (- (- extent) e) f))
+                         (t2 (/ (- extent e) f)))
+                     (declare (type single-float t1 t2))
+                     (when (> t1 t2)
+                       (rotatef t1 t2))
+                     (when (> t1 t-min)
+                       (setf t-min t1))
+                     (when (< t2 t-max)
+                       (setf t-max t2))
+                     (when (> t-min t-max)
+                       (return-from intersect-ray-obb-p nil))
+                     (when (< t-max 0.0f0)
+                       (return-from intersect-ray-obb-p nil)))
+                   (when (or (< (+ e extent) 0.0f0) (> (- e extent) 0.0f0))
+                     (return-from intersect-ray-obb-p nil))))))
+      t)))
